2c69214c68549b7e0746e8470ddfd40f32aa01bd
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-01-15 15:34:10 -0700

    back to basic

--- API/index.js
+++ API/index.js
@@ -1,36 +1,6 @@
 // serverless function
-
 // // import our express API
-// import app from '../server';
+import app from '../server';
 
 // // export our express API
-// export default app;
-
-const express = require("express");
-const app = express(); // setup up the server
-const port = process.env.PORT || 5000;
-const cors = require('cors');
-
-//solve cors issue when tsu website connects
-app.use(cors());
-
-
-// run the server
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-}
-)
-//Vercel rewrites all incoming requests to the "/API" 
-//To make these routes accessible through the Vercel deployment, they need to match the rewritten paths. 
-app.get('/api', (req, res) => {
-    res.send("server is up and running")
-    console.log('server is up and running!');
-})
-
-// import the routes
-const galleryRouter = require("./routes/gallery")
-
-// link the routes
-app.use("/api/gallery", galleryRouter)
-
-module.exports = app; // CommonJS syntax to export
+export default app;
